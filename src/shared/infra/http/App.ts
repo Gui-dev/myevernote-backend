25c3af0b214bf9d5831513217e08e884557b4e0c
Gui-dev <adrianon10@gmail.com>
2022-05-03 03:52:20 -0300

    feat: add handle errors

--- src/shared/infra/http/App.ts
+++ src/shared/infra/http/App.ts
@@ -1,6 +1,11 @@
+import 'reflect-metadata'
 import 'dotenv/config'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
 import cors from 'cors'
+
+import { AppError } from '@shared/error/AppError'
+import '@shared/container'
 
 import { routes } from './routes'
 
@@ -10,6 +15,21 @@ app.use(express.json())
 app.use(cors())
 
 app.use(routes)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      error: err.message
+    })
+  }
+  console.log(err)
+
+  return response.status(500).json({
+    status: 'error',
+    error: 'Internal Server Error'
+  })
+})
 
 export {
   app
