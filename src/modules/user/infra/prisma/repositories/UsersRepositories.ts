5014abe9187292c209c186632a75dbe554a0b4ae
Gui-dev <adrianon10@gmail.com>
2022-05-03 03:48:34 -0300

    feat: add repository UsersRepositories

---
+++ src/modules/user/infra/prisma/repositories/UsersRepositories.ts
@@ -0,0 +1,29 @@
+import { User } from '@prisma/client'
+
+import { IUsersRepositories } from '@modules/user/repositories/IUsersRepositories'
+import { prismaClient } from '@shared/prisma'
+import { ICreateUsersDTO } from '@modules/user/dtos/ICreateUsersDTO'
+
+export class UsersRepositories implements IUsersRepositories {
+  public async findUserByEmail (email: string): Promise<User | null> {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    return user
+  }
+
+  public async createUser ({ name, email, password }: ICreateUsersDTO): Promise<User> {
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password
+      }
+    })
+
+    return user
+  }
+}
