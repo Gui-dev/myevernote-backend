84eb7001b4e50f4eb1c545910063feed149c2886
Gui-dev <adrianon10@gmail.com>
2022-05-05 03:33:17 -0300

    feat: add heper method findUserById

--- src/modules/user/infra/prisma/repositories/UsersRepositories.ts
+++ src/modules/user/infra/prisma/repositories/UsersRepositories.ts
@@ -5,6 +5,15 @@ import { prismaClient } from '@shared/prisma'
 import { ICreateUsersDTO } from '@modules/user/dtos/ICreateUsersDTO'
 
 export class UsersRepositories implements IUsersRepositories {
+  public async findUserById (id: string): Promise<User | null> {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        id
+      }
+    })
+
+    return user
+  }
   public async findUserByEmail (email: string): Promise<User | null> {
     const user = await prismaClient.user.findUnique({
       where: {
--- src/modules/user/repositories/IUsersRepositories.ts
+++ src/modules/user/repositories/IUsersRepositories.ts
@@ -2,6 +2,7 @@ import { User } from '@prisma/client'
 import { ICreateUsersDTO } from '../dtos/ICreateUsersDTO'
 
 export interface IUsersRepositories {
+  findUserById (id: string): Promise<User | null>
   findUserByEmail (email: string): Promise<User | null>
   createUser (data: ICreateUsersDTO): Promise<User>
 }
