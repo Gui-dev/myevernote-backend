f0fb602fa88073292e812cfced15569cf0dd58a6
Gui-dev <adrianon10@gmail.com>
2022-05-06 03:50:33 -0300

    feat: add new route /notes

---
+++ src/modules/note/infra/http/routes/notes.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+
+import { NoteController } from '@modules/note/infra/http/controllers/NoteController'
+import { ensureAuthenticate } from '@modules/user/infra/http/middlewares/ensureAuthenticate'
+
+const notesRouter = Router()
+const noteController = new NoteController()
+
+notesRouter.post('/', ensureAuthenticate, noteController.create)
+
+export { notesRouter }
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from 'express'
 
 import { usersRoutes } from '@modules/user/infra/http/routes/users.routes'
 import { sessionsRoutes } from '@modules/user/infra/http/routes/sessions.routes'
+import { notesRouter } from '@modules/note/infra/http/routes/notes.routes'
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/users', sessionsRoutes)
+
+routes.use('/notes', notesRouter)
 
 export {
   routes
