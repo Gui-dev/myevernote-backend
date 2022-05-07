86b3a1063eda587a953fc666f94861ab9932d198
Gui-dev <adrianon10@gmail.com>
2022-05-07 02:01:15 -0300

    feat: add helper method show

--- src/modules/note/infra/http/controllers/NoteController.ts
+++ src/modules/note/infra/http/controllers/NoteController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 
 import { CreateNotesService } from '@modules/note/services/CreateNotesService'
+import { LookingNoteByIdService } from '@modules/note/services/LookingNoteByIdService'
 
 export class NoteController {
   public async create (request: Request, response: Response): Promise<Response> {
@@ -13,6 +14,15 @@ export class NoteController {
       title,
       body
     })
+
+    return response.status(201).json(note)
+  }
+
+  public async show (request: Request, response: Response): Promise<Response> {
+    const user_id = request.userId
+    const { id } = request.params
+    const lookingNoteByIdService = container.resolve(LookingNoteByIdService)
+    const note = await lookingNoteByIdService.execute(id, user_id)
 
     return response.status(201).json(note)
   }
