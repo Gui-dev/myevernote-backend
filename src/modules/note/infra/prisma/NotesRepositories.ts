15b6b9a02dbc01fad9c925d96a5515f74e36f162
Gui-dev <adrianon10@gmail.com>
2022-05-07 01:59:50 -0300

    feat: add helper method lookingNoteById

--- src/modules/note/infra/prisma/NotesRepositories.ts
+++ src/modules/note/infra/prisma/NotesRepositories.ts
@@ -1,9 +1,33 @@
+import { prismaClient } from '@shared/prisma'
+
 import { INotesRepositories } from '@modules/note/repositories/INotesRepositories'
 import { ICreateNoteDTO } from '@modules/note/dtos/ICreateNoteDTO'
-import { prismaClient } from '@shared/prisma'
 import { INoteResponseDTO } from '@modules/note/dtos/INoteResponseDTO'
 
 export class NotesRepositories implements INotesRepositories {
+  public async lookingNoteById (id: string): Promise<INoteResponseDTO | null> {
+    const note = await prismaClient.note.findUnique({
+      where: {
+        id
+      },
+      select: {
+        id: true,
+        title: true,
+        body: true,
+        created_at: true,
+        author: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            created_at: true
+          }
+        }
+      }
+    })
+
+    return note
+  }
   public async createNote ({ user_id, title, body }: ICreateNoteDTO): Promise<INoteResponseDTO> {
     const note = await prismaClient.note.create({
       data: {
--- src/modules/note/repositories/INotesRepositories.ts
+++ src/modules/note/repositories/INotesRepositories.ts
@@ -3,4 +3,5 @@ import { INoteResponseDTO } from '../dtos/INoteResponseDTO'
 
 export interface INotesRepositories {
   createNote (data: ICreateNoteDTO): Promise<INoteResponseDTO>
+  lookingNoteById(id: string): Promise<INoteResponseDTO | null>
 }
