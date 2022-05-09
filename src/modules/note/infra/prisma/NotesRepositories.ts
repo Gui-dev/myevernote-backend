7de99fafd9dd705fde6118b3725a8f3ccf86d740
Gui-dev <adrianon10@gmail.com>
2022-05-09 03:26:13 -0300

    feat: add helper method updateNoteById

--- src/modules/note/infra/prisma/NotesRepositories.ts
+++ src/modules/note/infra/prisma/NotesRepositories.ts
@@ -3,6 +3,7 @@ import { prismaClient } from '@shared/prisma'
 import { INotesRepositories } from '@modules/note/repositories/INotesRepositories'
 import { ICreateNoteDTO } from '@modules/note/dtos/ICreateNoteDTO'
 import { INoteResponseDTO } from '@modules/note/dtos/INoteResponseDTO'
+import { IUpdateNoteDTO } from '@modules/note/dtos/IUpdateNoteDTO'
 
 export class NotesRepositories implements INotesRepositories {
   public async lookingNoteById (id: string): Promise<INoteResponseDTO | null> {
@@ -59,6 +60,34 @@ export class NotesRepositories implements INotesRepositories {
         title,
         body,
         user_id
+      },
+      select: {
+        id: true,
+        title: true,
+        body: true,
+        created_at: true,
+        author: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            created_at: true
+          }
+        }
+      }
+    })
+
+    return note
+  }
+
+  public async updateNoteById ({ id, title, body }: IUpdateNoteDTO): Promise<INoteResponseDTO> {
+    const note = await prismaClient.note.update({
+      where: {
+        id
+      },
+      data: {
+        title,
+        body
       },
       select: {
         id: true,
--- src/modules/note/repositories/INotesRepositories.ts
+++ src/modules/note/repositories/INotesRepositories.ts
@@ -1,8 +1,10 @@
 import { ICreateNoteDTO } from '../dtos/ICreateNoteDTO'
 import { INoteResponseDTO } from '../dtos/INoteResponseDTO'
+import { IUpdateNoteDTO } from '../dtos/IUpdateNoteDTO'
 
 export interface INotesRepositories {
   createNote (data: ICreateNoteDTO): Promise<INoteResponseDTO>
   lookingNoteById(id: string): Promise<INoteResponseDTO | null>
   listAllNotes(user_id: string): Promise<INoteResponseDTO[] | null>
+  updateNoteById(data: IUpdateNoteDTO): Promise<INoteResponseDTO>
 }
