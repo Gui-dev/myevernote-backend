d5cd8b5c4b134439caedcf9a3b589705def59371
Gui-dev <adrianon10@gmail.com>
2022-05-11 03:28:16 -0300

    feat: add controller SearchNoteController

---
+++ src/modules/note/infra/http/controllers/SearchNoteController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { SearchNoteByWordService } from '@modules/note/services/SearchNoteByWordService'
+
+export class SearchNoteController {
+  public async index (request: Request, response: Response): Promise<Response> {
+    const user_id = request.userId
+    const { query } = request.query
+    const searchNoteByWordService = container.resolve(SearchNoteByWordService)
+    const notes = await searchNoteByWordService.execute(String(query), user_id)
+    console.log('Aqui')
+    return response.status(201).json(notes)
+  }
+}
