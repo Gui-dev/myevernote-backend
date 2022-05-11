17d8c5191dc0d1660748af78fce9e0060682b9c4
Gui-dev <adrianon10@gmail.com>
2022-05-11 03:34:22 -0300

    feat: add new route get /notes/search

--- src/modules/note/infra/http/routes/notes.routes.ts
+++ src/modules/note/infra/http/routes/notes.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 
 import { NoteController } from '@modules/note/infra/http/controllers/NoteController'
+import { SearchNoteController } from '@modules/note/infra/http/controllers/SearchNoteController'
 import { ensureAuthenticate } from '@modules/user/infra/http/middlewares/ensureAuthenticate'
 
 const notesRouter = Router()
 const noteController = new NoteController()
+const searchNoteController = new SearchNoteController()
 
 notesRouter.post('/', ensureAuthenticate, noteController.create)
 notesRouter.get('/', ensureAuthenticate, noteController.index)
+notesRouter.get('/search', ensureAuthenticate, searchNoteController.index)
 notesRouter.get('/:id', ensureAuthenticate, noteController.show)
 notesRouter.put('/:id', ensureAuthenticate, noteController.update)
 notesRouter.delete('/:id', ensureAuthenticate, noteController.delete)
