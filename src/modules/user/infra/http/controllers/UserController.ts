36af07936e0fdb5e02e3f8fc2ceb7b016f4868d1
Gui-dev <adrianon10@gmail.com>
2022-06-14 03:48:24 -0300

    feat: add helper method update

--- src/modules/user/infra/http/controllers/UserController.ts
+++ src/modules/user/infra/http/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 
 import { CreateUserService } from '@modules/user/services/CreateUsersService'
+import { UpdatePersonalInformationService } from '@modules/user/services/UpdatePersonalInformationService'
 
 export class UserController {
   public async create (request: Request, response: Response): Promise<Response> {
@@ -13,6 +14,18 @@ export class UserController {
       password
     })
 
+    return response.status(201).json(user)
+  }
+
+  public async update (request: Request, response: Response): Promise<Response> {
+    const id = request.userId
+    const { name, email } = request.body
+    const updatePersonalInformationService = container.resolve(UpdatePersonalInformationService)
+    const user = await updatePersonalInformationService.execute({
+      id,
+      name,
+      email
+    })
     return response.status(201).json(user)
   }
 }
