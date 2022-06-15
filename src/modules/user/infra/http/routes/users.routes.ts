a2aafe41366ff8a5238e2370086943510623c131
Gui-dev <adrianon10@gmail.com>
2022-06-15 03:57:29 -0300

    feat: add new route /users/reset_password

--- src/modules/user/infra/http/routes/users.routes.ts
+++ src/modules/user/infra/http/routes/users.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 
 import { UserController } from '@modules/user/infra/http/controllers/UserController'
+import { ResetPasswordController } from '@modules/user/infra/http/controllers/ResetPasswordController'
 import { ensureAuthenticate } from '@modules/user/infra/http/middlewares/ensureAuthenticate'
 
 const usersRoutes = Router()
 const userController = new UserController()
+const resetPasswordController = new ResetPasswordController()
 
 usersRoutes.post('/register', userController.create)
 usersRoutes.put('/edit', ensureAuthenticate, userController.update)
+usersRoutes.put('/reset_password', ensureAuthenticate, resetPasswordController.update)
 
 export { usersRoutes }
