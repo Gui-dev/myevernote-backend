bff05e0cf68aa3a007d3cc9ed8ffa4be68233e98
Gui-dev <adrianon10@gmail.com>
2022-06-15 03:55:20 -0300

    feat: add interface IUsersRepositories

--- src/modules/user/repositories/IUsersRepositories.ts
+++ src/modules/user/repositories/IUsersRepositories.ts
@@ -1,10 +1,12 @@
 import { User } from '@prisma/client'
 import { ICreateUsersDTO } from '../dtos/ICreateUsersDTO'
 import { IUpdatePersonalInformationDTO } from '../dtos/IUpdatePersonalInformationDTO'
+import { IUpdateUserPasswordDTO } from '../dtos/IUpdateUserPasswordDTO'
 
 export interface IUsersRepositories {
   findUserById (id: string): Promise<User | null>
   findUserByEmail (email: string): Promise<User | null>
   createUser (data: ICreateUsersDTO): Promise<User>
   updatePersonalInformation (data: IUpdatePersonalInformationDTO): Promise<User>
+  updateUserPassword(data: IUpdateUserPasswordDTO): Promise<User>
 }
